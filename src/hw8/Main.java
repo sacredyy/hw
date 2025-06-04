790d47b7269d3e4cd9c2c2b80fc475369a87b3c6
whysacredyy27 <whysacredyy27s@gmail.com>
2025-06-04 22:04:58 +0200

    hw3

--- src/hw8/Main.java
+++ src/hw8/Main.java
@@ -1,19 +1,26 @@
 package hw8;
 
 import java.util.List;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import static hw8.NetService.getLinksFromUrl;
 
 public class Main {
     public static void main(String[] args) {
         String spec = "https://dou.ua/";
+        String outputFile = "links.txt";
         try {
             List<String> links = getLinksFromUrl(spec, "UTF-8");
-            System.out.println("links " + links.size());
-            for (String link : links) {
-                System.out.println(link);
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+                for (String link : links) {
+                    writer.write(link);
+                    writer.newLine();
+                }
+                System.out.println("succes");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
